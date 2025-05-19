50020686bf3b4379ef17bad4a2d2b2af8b4b678e
김연주 <75323528+yeonjoo7@users.noreply.github.com>
2025-05-20 00:30:48 +0900

    Title: 최소공배수 구하기, Time: 188ms, Memory: 17MB, Status: Wrong Answer - Codetree

---
+++ 250520/최소공배수 구하기/find-the-least-common-multiple.js
@@ -0,0 +1,24 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split('\n');
+
+let [n, m] = input[0].split(' ').map(Number);
+
+// Please Write your code here.
+const nDivisor = [];
+const mDivisor = [];
+const largerNumber = n > m? n : m;
+for (let i=2; i<largerNumber; i++) {
+    if (i < n && n % i == 0) {
+        nDivisor.push(i)
+    }
+    if (i < m && m % i == 0) {
+        mDivisor.push(i)
+    }
+}
+const uniqueNumbers = nDivisor.filter((d) => !mDivisor.includes(d))
+mDivisor.forEach((d) => !nDivisor.includes(d) ? uniqueNumbers.push(d) : undefined)
+
+const result = uniqueNumbers.reduce((acc, curr)=> 
+    acc *= curr
+, 1)
+console.log(result)
